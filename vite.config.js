895aa56b79ff221984f55437299870bf11470378
sahritika07 <ritikasah731@gmail.com>
2026-09-21 20:31:00 +0530

    Deploying react app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.js']
+  },
   base: '/Credit-Wallet-Frontend/'
 })
